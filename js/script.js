b6c04c10e27b2cfeb26dadd374245989b7a319b1
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-09 16:40:21 +0200

    aggiunta richiesta numeri e fix vari

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 // TRACCIA DELL'ESERCIZIO
+
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
 // I numeri non possono essere duplicati.
+
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+
 // L’utente non può inserire più volte lo stesso numero.
+
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+// BONUS: (da fare solo se funziona tutto il resto)
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 => tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
 
 //------ FUNCTIONS ------//
 function getRandomNumber (min , max) {
@@ -30,9 +42,6 @@ function getMatch (firstArray, secondArray) {
 const mineField = [];
 for (let i = 1 ; i <= 100 ; i++) {
      mineField.push(i);
-     for (let j = 1 ; j <= 100 ; j++) {
-
-     }
 }
  console.log(mineField);
 
@@ -50,12 +59,7 @@ console.log(cpuMines);
 getMatch(mineField,cpuMines);
 
 // 4. CHIEDO AL GIOCATORE DI INSERIRE UN NUMERO
-let userChoice = Number(prompt("Inserisci un numero"));
-console.log(userChoice);
-
-
-// BONUS: (da fare solo se funziona tutto il resto)
-// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
-// con difficoltà 0 => tra 1 e 100
-// con difficoltà 1 => tra 1 e 80
-// con difficoltà 2 => tra 1 e 50
+do {
+    let userChoice = Number(prompt("Inserisci un numero"));
+    console.log(userChoice);
+} while (getMatch(mineField,cpuMines) === false);
